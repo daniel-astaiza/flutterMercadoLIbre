67d16cc4260290ab5be7bc0b1afefad750963321
daniel-astaiza <danielastaiza111@gmail.com>
2024-08-08 11:11:39 -0500

    se acomodo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors"); // Asegúrate de añadir esta línea
 require("dotenv").config();
-const userRoutes = require("./routes/user")
-const categorieRoutes = require("./routes/categorie")
-const productsRoutes = require("./routes/products")
+const userRoutes = require("./routes/user");
+const categorieRoutes = require("./routes/categorie");
+const productsRoutes = require("./routes/products");
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -14,27 +15,22 @@ app.use((req, res, next) => {
     next();
 });
 
-// middlewre
-app.use(cors());
-app.use(express.json())
+// Middleware
+app.use(cors()); // Usa el middleware CORS
+app.use(express.json());
 
-app.use("/api",userRoutes);
-app.use("/api",categorieRoutes);
-app.use("/api",productsRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categorieRoutes);
+app.use("/api", productsRoutes);
 
-
-
-// routes
-
-app.get ("/",(req, res) => {
-    res.send("¡Bienvenidos!")
+// Rutas
+app.get("/", (req, res) => {
+    res.send("¡Bienvenidos!");
 });
 
-//mongodb conection
+// Conexión a MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("Connected to MongoDB Atlas"))
+    .catch((error) => console.error(error));
 
-mongoose.connect(
-    process.env.MONGODB_URI)
-    .then(()=>console.log("Connected to MongoDB Atlas"))
-    .catch((error)=>console.error(error))
-
-app.listen(port, () => console.log ("server listening on port", port));
+app.listen(port, () => console.log("Server listening on port", port));
